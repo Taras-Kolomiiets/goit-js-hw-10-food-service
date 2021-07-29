032038a3b196b8fd2ede3f4c480ae63a949377a8
Taras-Kolomiiets <taraskolomiiets8991@gmail.com>
2021-07-29 16:21:12 +0300

    Add change Theme

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ const menuRef = document.querySelector('.js-menu');
 
 const menuMarkup = menuTemplate(menu);
 menuRef.insertAdjacentHTML('beforeend', menuMarkup);
-console.log(`menuMarkup`, menuMarkup)
+
+const themeInput = document.querySelector('#theme-switch-toggle');
+
+themeInput.addEventListener('change', onThemeChange);
+
+
+function onThemeChange(e) {
+  if (e.currentTarget.checked) {
+    document.body.classList.add('dark-theme')
+  } else {
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+  }
+}
 
 
